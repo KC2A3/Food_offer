ac46acdba2babc637225388e93cb1fa1b576acd4
KC2A3 <947496351@qq.com>
2018-12-24 00:38:30 +0800

    make tasking

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,19 @@
+'use strict';
+var loadAllItems = require("items.js");
+var loadPromotions = require("promotions");
+
 function bestCharge(selectedItems) {
   return /*TODO*/;
 }
+
+/*读取输入的菜品数量*/
+function getNumber{}；
+/*根据菜品数量算出各个菜品价格*/
+function getPrice{}
+/*满30元的话进行满30减6操作*/
+function discount{}
+/*进行指定菜品半价操作*/
+function halfPrice{}
+/*比较两种方法优惠量大小并选择*/
+/*输出商品清单和优惠信息*/
+function printList{}
